cd239d45b0dc9255f3eecdfbff963447b50a5601
sw-yx <swyx@dontemail.me>
2019-05-17 03:24:31 -0400

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Theme Dev Blog`,
+    description: `Write your dev blog with no graphql!`,
+    author: `@swyx`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
+  __experimentalThemes: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-theme-dev-blog",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        unusedOptionForNow: 5,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
